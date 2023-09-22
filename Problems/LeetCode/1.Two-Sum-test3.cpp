c93fb24f24662bfce871a9eb1d66af6e57d33f1f
Ismail El-Toukhy <44483092+Ismail-Mahmoud@users.noreply.github.com>
2023-09-22 11:59:01 +0300

    (test3) add LeetCode 1

---
+++ Problems/LeetCode/1.Two-Sum-test3.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> idx;
+        for(int i = 0; i < nums.size(); ++i) {
+            auto it = idx.find(target - nums[i]);
+            if(it != idx.end()) {
+                return {it->second, i};
+            }
+            idx[nums[i]] = i;
+
+        }
+        assert(false);
+        return {};
+    }
+};
