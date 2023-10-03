e152958bb71857832d27f9db65e49cb515190014
Ismail El-Toukhy <44483092+Ismail-Mahmoud@users.noreply.github.com>
2023-10-03 11:24:32 +0300

    add LeetCode 1512 (by lcws)

---
+++ Problems/LeetCode/1512.Number-of-Good-Pairs.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/number-of-good-pairs/
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int ans = 0;
+        vector<int> freq(101, 0);
+        for(int x : nums) {
+            ans += freq[x]++;
+        }
+        return ans;
+    }
+};
+
