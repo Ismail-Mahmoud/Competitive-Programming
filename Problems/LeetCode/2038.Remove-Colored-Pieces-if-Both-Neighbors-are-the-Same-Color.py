0d9ee45a93a64a0e337122f87091c21254824d9e
Ismail El-Toukhy <44483092+Ismail-Mahmoud@users.noreply.github.com>
2023-10-03 11:18:33 +0300

    add LeetCode 2038 (by lcws)

---
+++ Problems/LeetCode/2038.Remove-Colored-Pieces-if-Both-Neighbors-are-the-Same-Color.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        moves = dict(A=0, B=0)
+        for i in range(1, len(colors) - 1):
+            moves[colors[i]] += colors[i - 1] == colors[i] == colors[i + 1]
+        return moves["A"] > moves["B"]
+
